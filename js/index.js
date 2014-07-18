575778c7eaed4cef136c17113cf27e34e61f412a
Gocho Mugo <gochomugo.developer@gmail.com>
2014-07-18 17:13:09 +0300

    Updating JS

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ $(".markdown").each(function () {
     markdown = markdown.replace(/`{3}(\w+)\n/g, "<pre><code class='language-$1'>");
     markdown = markdown.replace(/`{3}\n/g, "</code></pre><br>");
     markdown = markdown.replace(/\n`{1}(.*)`{1}/g, "<br><pre><code class='language-python'>$1</code></pre><br>")
+    markdown = markdown.replace(/`{1}([\w]*)`{1}/g, "<code class='language-python'>$1</code>");
 
    html = converter.makeHtml(markdown);
 
