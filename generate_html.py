7c6fef5f7e3b6e0fdd0a03132e7e5b09c60fd141
Gocho Mugo <gochomugo.developer@gmail.com>
2014-07-18 17:42:38 +0300

    Better Organisation

--- generate_html.py
+++ generate_html.py
@@ -1,5 +1,5 @@
 '''
-Generates HTML Using the README and template in the Directory
+Generates HTML Using the markdown files and templates in the Directory
 '''
 
 import os
@@ -16,20 +16,25 @@ def write_file(path, content):
         my_file.write(content)
 
 
-def generate_html():
-    template = read_file('template.html')
-    readme = read_file('README.md')
-    version = re.search(r'Version.*(\d.\d.\d)', readme).group(1)
-    status = re.search(r'Status[.\|\s]*(\w.*\w)[\s\|]*', readme).group(1)
-    last_updated = re.search(r'Updated[.\|\s]*(\w.*\w)[\s\|]*', readme).group(1)
-    new_content = readme[readme.index("## Prerequisites"):]
+def generate_html(template, markdown, html, formatting):
+    template = read_file(template)
+    raw = read_file(markdown)
+    template = formatting(template, raw)
+    write_file(html, template)
+
+def index(template, markdown):
+    version = re.search(r'Version.*(\d.\d.\d)', markdown).group(1)
+    status = re.search(r'Status[.\|\s]*(\w.*\w)[\s\|]*', markdown).group(1)
+    last_updated = re.search(r'Updated[.\|\s]*(\w.*\w)[\s\|]*', markdown).group(1)
+    new_content = markdown[markdown.index("## Prerequisites"):]
     template = template.format(
         version=version,
         status=status,
         last_updated=last_updated,
         content=new_content
     )
-    write_file('index.html', template)
+    return template
 
 if __name__ == '__main__':
-    generate_html()
+    generate_html('templates/index.html', 'docs/index.md', 'index.html', index)
+    # generate_html('templates/changes.html', 'docs/changes.md', 'changes.html')
